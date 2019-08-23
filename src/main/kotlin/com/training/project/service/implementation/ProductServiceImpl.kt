bbfee1b5270001bdcdfa43a52a0034978d8cb02b
Sunny Bangale <sunnybangale@gmail.com>
2019-08-23 16:40:00 -0700

    Service, Domain model and Events with its corresponding tests
    * Initial model
    
    * Added classes and interfaces for model
    
    * Added classes and interfaces for model
    
    * Tests added for product, price and product service implementation
    
    * Code cleanup for tests
    
    * Commeting events until the appropriate tests be done
    
    * Adding tests for events. Refactoring unit and integration tests
    
    * Inspections and warnings fixed

--- src/main/kotlin/com/training/project/service/implementation/ProductServiceImpl.kt
+++ src/main/kotlin/com/training/project/service/implementation/ProductServiceImpl.kt
@@ -4,32 +4,44 @@ import com.training.project.service.ProductService
 import com.training.project.service.model.Currency
 import com.training.project.service.model.Price
 import com.training.project.service.model.Product
+import com.training.project.service.model.ProductDeregistered
+import com.training.project.service.model.ProductEvent
+import com.training.project.service.model.ProductRegistered
 import com.training.project.service.model.ProductRepository
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import java.util.UUID
 import javax.transaction.Transactional
 
 @Service
 @Transactional
-class ProductImpl(private val repository: ProductRepository) : ProductService {
+class ProductServiceImpl(private val repository: ProductRepository, private val publish: ApplicationEventPublisher) : ProductService {
 
     override fun product(productId: UUID): Product = repository.findById(productId).get()
     override fun products(): List<Product> = repository.findAll().toList()
-    override fun registerProduct(name: String, currency: Currency, amount: Int): Product = repository.save(Product(name, Price(currency, amount)))
-    override fun deregisterProduct(productId: UUID) = repository.deleteById(productId)
-    override fun increasePrice(productId: UUID, percentage: Int) {
-        val foundProduct = product(productId)
-        foundProduct.increasePrice(percentage)
-        repository.save(foundProduct)
 
+    override fun registerProduct(name: String, currency: Currency, amount: Double): Product = repository.save(Product(name, Price(currency, amount))).also { publish(ProductRegistered(it.productId, it.name, it.price)) }
+
+    override fun deregisterProduct(productId: UUID) = repository.deleteById(productId).also { publish(ProductDeregistered(productId)) }
+
+    override fun setPrice(productId: UUID, currency: Currency, amount: Double) {
+        val product = product(productId)
+        product.setPrice(currency, amount)
+        repository.save(product)
         //ToDo() Mutation function for optimization
     }
 
-    override fun decreasePrice(productId: UUID, percentage: Int) {
+    override fun increasePrice(productId: UUID, percentage: Double) {
+        val foundProduct = product(productId)
+        foundProduct.increasePrice(percentage)
+        repository.save(foundProduct)
+        //ToDo() Mutation function for optimization
+    }
+
+    override fun decreasePrice(productId: UUID, percentage: Double) {
         val foundProduct = product(productId)
         foundProduct.decreasePrice(percentage)
         repository.save(foundProduct)
-
         //ToDo() Mutation function for optimization
     }
 
@@ -37,7 +49,8 @@ class ProductImpl(private val repository: ProductRepository) : ProductService {
         val foundProduct = product(productId)
         foundProduct.rename(name)
         repository.save(foundProduct)
-
         //ToDo() Mutation function for optimization
     }
+
+    private fun publish(event: ProductEvent) = publish.publishEvent(event)
 }
--- src/main/kotlin/com/training/project/service/model/Product.kt
+++ src/main/kotlin/com/training/project/service/model/Product.kt
@@ -1,8 +1,9 @@
 package com.training.project.service.model
 
+import com.google.common.base.Preconditions.checkArgument
+import com.training.project.service.infrastructure.AggregateRoot
 import com.training.project.service.model.Currency.DOLLAR
 import java.util.UUID
-import javax.persistence.Embeddable
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -10,35 +11,44 @@ import javax.persistence.Id
 @Entity
 data class Product(
         var name: String = "",
-        var price: Price = Price(DOLLAR, 0),
+        var price: Price = Price(DOLLAR, 0.0),
         @Id @GeneratedValue
         val productId: UUID = UUID.randomUUID()
-) {
+) : AggregateRoot<ProductEvent>() {
 
-    fun rename(name: String) {
-        if (this.name != name) {
-            this.name = name
+    fun rename(newName: String) {
+        checkArgument(newName.isNotEmpty())
+        if (name != newName) {
+            publish(ProductRenamed(productId, newName, name))
+            name = newName
         }
     }
 
-    fun setPrice() = println()
-
-    fun increasePrice(percentage: Int) {
-        if (percentage != 0) {
-            val amount = price.amount * (1 + percentage / 100)
-            price = Price(price.currency, amount)
+    fun setPrice(currency: Currency, amount: Double) {
+        checkArgument(amount > 0)
+        setNewPrice(Price(currency, amount))?.let {
+            publish(ProductPriceSet(productId, it.first, it.second))
         }
     }
 
-    fun decreasePrice(percentage: Int) {
-        if (percentage != 0) {
-            val amount = price.amount * (1 - percentage / 100)
-            price = Price(price.currency, amount)
+    fun increasePrice(percentage: Double) {
+        checkArgument(percentage > 0)
+        setNewPrice(price.applyPercentage(percentage))?.let{
+            publish(ProductPriceIncreased(productId, it.first, it.second))
         }
     }
+
+    fun decreasePrice(percentage: Double) {
+        checkArgument(percentage > 0)
+        setNewPrice(price.applyPercentage(percentage * -1))?.let{
+            publish(ProductPriceDecreased(productId, it.first, it.second))
+        }
+    }
+
+    private fun setNewPrice(price: Price): Pair<Price, Price>? =
+            if (this.price != price) {
+                val oldPrice = this.price
+                this.price = price
+                Pair(price, oldPrice)
+            } else null
 }
-
-@Embeddable
-data class Price(val currency: Currency, val amount: Int)
-
-enum class Currency { DOLLAR, PESOS, RUPEES }
