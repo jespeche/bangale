8cd43ad5d80c377773bb8b7fe3c62cf1f2379d09
jespeche <javier.augusto.espeche@oracle.com>
2019-08-27 19:54:27 -0700

    Updating dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
-    kotlin("plugin.jpa") version "1.3.41"
-    kotlin("jvm") version "1.3.41"
-    kotlin("plugin.spring") version "1.3.41"
+    kotlin("plugin.jpa") version "1.3.50"
+    kotlin("jvm") version "1.3.50"
+    kotlin("plugin.spring") version "1.3.50"
     jacoco
 }
 
@@ -21,6 +21,7 @@ configurations {
 }
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
@@ -29,14 +30,14 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:5.9.0")
-    implementation("com.graphql-java-kickstart:graphql-java-servlet:7.5.0")
-    implementation("com.graphql-java-kickstart:graphql-java-tools:5.4.0")
-    implementation("com.graphql-java:graphql-java:11.0")
+    implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:5.10.0")
+    implementation("com.graphql-java-kickstart:graphql-java-servlet:8.0.0")
+    implementation("com.graphql-java-kickstart:graphql-java-tools:5.6.0")
+    implementation("com.graphql-java:graphql-java:13.0")
     implementation("com.graphql-java:graphql-java-extended-scalars:1.0")
 
-    runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:5.9.0") { because("Embedded Graphql IDE") }
-    runtimeOnly("com.graphql-java-kickstart:voyager-spring-boot-starter:5.9.0") { because("Embedded schema navigation") }
+    runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:5.10.0") { because("Embedded Graphql IDE") }
+    runtimeOnly("com.graphql-java-kickstart:voyager-spring-boot-starter:5.10.0") { because("Embedded schema navigation") }
     runtimeOnly("com.h2database:h2") { because("In memory database") }
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -45,7 +46,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
     testImplementation("org.assertj:assertj-core:3.12.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-    testCompile("com.graphql-java-kickstart:graphql-spring-boot-starter-test:5.9.0")
+    testImplementation("com.graphql-java-kickstart:graphql-spring-boot-starter-test:5.10.0")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools") { because("Fast reload and H2 console") }
 }
